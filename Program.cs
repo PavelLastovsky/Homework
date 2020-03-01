9e64fdb5c783837d6acd444409e3ae8b56904af2
PavelLastovsky <61546914+PavelLastovsky@users.noreply.github.com>
2020-03-02 01:06:18 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+namespace task_DEV1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int count = 1;
+            int finalcount = 0;
+            Console.WriteLine("Enter string");
+            string yourstring = Console.ReadLine();
+            int length = yourstring.Length;
+
+            for (int i = 0; i < length; i++)
+
+            {
+
+                if ( i < length - 1 && yourstring[i] == yourstring[i + 1])
+                    count++;
+
+                else
+                {
+
+                    if (count > finalcount)
+                    {
+                        finalcount = count;
+                    }
+
+                    count = 1;
+                }
+            }
+
+            Console.WriteLine($"maximum number of matches per line: {finalcount}");
+            Console.ReadKey();
+        }
+    }
+    }
