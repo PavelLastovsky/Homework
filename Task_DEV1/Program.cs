cd0593fb7caa871569d6bb59ad82d88400bde319
Lastovsky <lastovsky.pavel@mail.ru>
2020-03-16 15:37:29 +0300

    DEV1

---
+++ Task_DEV1/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task_DEV1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter string");
+            string YourString = Console.ReadLine();
+            Console.WriteLine($"Maximum number of unmatches per line: {SequenceOfElements.MaxDifferentElements(YourString)}") ;
+            Console.ReadKey();
+        }
+    }
+}
