f870e447b7fd6ef39d9477567dc5e9034ec20d84
Lastovsky <lastovsky.pavel@mail.ru>
2020-03-16 15:00:44 +0300

    DEV1

--- Program.cs
+++ Program.cs
@@ -1,37 +1,19 @@
 ﻿using System;
-namespace task_DEV1
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task_DEV1
 {
     class Program
     {
         static void Main(string[] args)
         {
-            int count = 1;
-            int finalcount = 0;
             Console.WriteLine("Enter string");
-            string yourstring = Console.ReadLine();
-            int length = yourstring.Length;
-
-            for (int i = 0; i < length; i++)
-
-            {
-
-                if ( i < length - 1 && yourstring[i] == yourstring[i + 1])
-                    count++;
-
-                else
-                {
-
-                    if (count > finalcount)
-                    {
-                        finalcount = count;
-                    }
-
-                    count = 1;
-                }
-            }
-
-            Console.WriteLine($"maximum number of matches per line: {finalcount}");
+            string YourString = Console.ReadLine();
+            Console.WriteLine($"Maximum number of unmatches per line: {SequenceOfElements.MaxDifferentElements(YourString)}") ;
             Console.ReadKey();
         }
     }
-    }
+}
---
+++ UnitTest1.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Task_DEV1;
+
+namespace UnitTestDEV1
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void CheckForCurrentWorkMaxDifferentElements()
+        {
+            int CheckValueForCurrentWork = 8;
+            string exampleString = "aabcdefghh";
+            Assert.AreEqual(SequenceOfElements.MaxDifferentElements(exampleString), CheckValueForCurrentWork);
+        }
+        [TestMethod]
+        public void CheckForEmptyStringMaxDifferentElements()
+        {
+            int CheckValueForEmptyString = 0;
+            string ExampleEmptyString = "";
+            Assert.AreEqual(SequenceOfElements.MaxDifferentElements(ExampleEmptyString), CheckValueForEmptyString);
+        }
+        [TestMethod]
+        public void CheckForOneElementStringMaxDifferentElements()
+        {
+            int CheckValueForOneElementString = 1;
+            string ExampleOneElementString = "A";
+            Assert.AreEqual(SequenceOfElements.MaxDifferentElements(ExampleOneElementString), CheckValueForOneElementString);
+
+        }
+        [TestMethod]
+        public void CheckForNullStringMaxDifferentElements()
+        {
+            int CheckValueForNullString = 0;
+            string ExampleNullString = null;
+            Assert.AreEqual(SequenceOfElements.MaxDifferentElements(ExampleNullString), CheckValueForNullString);
+
+        }
+    }
+}
