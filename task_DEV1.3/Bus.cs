0f71b6b019d918b4dae1fdce50b8365c61cb1877
Lastovsky <lastovsky.pavel@mail.ru>
2020-03-23 21:29:49 +0300

    DEV 1.3

---
+++ task_DEV1.3/Bus.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace task_DEV1._3
+{
+    public class Bus : Vehicle
+    {
+        const string _vehicleType = "Bus";
+        public int _passengersQuantity;
+
+        public int PassengersQuantity
+        {
+            get 
+            { 
+                return _passengersQuantity; 
+            }
+            set
+            {
+                _passengersQuantity = value;
+            }
+        }
+        /// <summary>
+        /// Bus constructor that determined Passengers quantity
+        /// </summary>
+        /// <param name="PassengersQuantity"> Passengers quantity in Bus</param>
+        /// <param name="engine"> engine parameters in Bus</param>
+        /// <param name="chassis"> chassic parameters in Bus </param>
+        /// <param name="transmission"> transmission parameters in Bus </param>
+        public Bus(int PassengersQuantity, Engine engine, Chassis chassis, Transmission transmission) : base(engine, chassis, transmission, _vehicleType)
+        {
+            this.PassengersQuantity = PassengersQuantity;
+        }
+        new public void GetInfo()
+        {
+            Console.WriteLine($"Passengers quantity in bus is {_passengersQuantity}");
+            base.GetInfo();
+        }
+    }
+}
---
+++ task_DEV1.3/chassis.cs
@@ -0,0 +1,79 @@
+﻿using System;
+
+namespace task_DEV1._3
+{
+   public class Chassis
+    {
+        int _wheelsQuantity;
+        string _chassisNumber;
+        double _permissibleLoad;
+
+        public int WheelsQuantity
+        {
+            get 
+            { 
+                return _wheelsQuantity; 
+            }
+            set
+            {
+                _wheelsQuantity = value;
+            }
+        }
+        public string ChassisNumber
+        {
+            get 
+            { 
+                return _chassisNumber; 
+            }
+            set
+            {
+                NullStringCheck(value);
+                EmptyStringCheck(value);
+                _chassisNumber = value;
+            }
+        }
+        public double PermissibleLoad
+        {
+            get 
+            { 
+                return _permissibleLoad; 
+            }
+            set
+            {
+                _permissibleLoad = value;
+            }
+        }
+        /// <summary>
+        /// Chassis constructor
+        /// </summary>
+        /// <param name="WheelsQuantity"> Quantity of wheels </param>
+        /// <param name="ChassisNumber"> </param>
+        /// <param name="PermissibleLoad"></param>
+        public Chassis(int WheelsQuantity, string ChassisNumber, double PermissibleLoad)
+        {
+            this.WheelsQuantity = WheelsQuantity;
+            this.ChassisNumber = ChassisNumber;
+            this.PermissibleLoad = PermissibleLoad;
+        }
+        public void GetInfo()
+        {
+            Console.WriteLine($"Chassis number of wheels is: {_wheelsQuantity}");
+            Console.WriteLine($"Chassis serial number is: {_chassisNumber}");
+            Console.WriteLine($"Chassis permissible load is: {_permissibleLoad}");
+        }
+        public void NullStringCheck(object value)
+        {
+            if (value == null)
+            {
+                throw new ArgumentNullException("You entered null value");
+            }
+        }
+        public void EmptyStringCheck(object value)
+        {
+            if ((string)value == string.Empty)
+            {
+                throw new FormatException("You entered empty string");
+            }
+        }
+    }
+}
